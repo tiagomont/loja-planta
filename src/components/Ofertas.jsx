b2ff6f676ce38bff6e592ca42b6240cafb011d73
Tiago Monteiro Alves <tiagovga456@gmail.com>
2024-11-26 20:51:47 -0300

    feat Ofertas: criando a sessão de ofertas

--- src/components/Ofertas.jsx
+++ src/components/Ofertas.jsx
@@ -1,10 +1,16 @@
 export default function Ofertas() {
+    cosnt [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch()
+    });
+
     return (
         <div className="ofertas">
             <h1>
-                <span>Conheça nossas</span>
+                <span className="sub-h1">Conheça nossas</span>
                 <br />
-                <span>Ofertas</span>
+                <span className="principal-h1">Ofertas</span>
             </h1>
             <div className="contain-items">
                 
